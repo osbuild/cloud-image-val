1362818d077fb189d515b43ca2198632e6b91f4d
Nicolás Muñoz <nmunoz@redhat.com>
2022-04-22 17:48:44 +0200

    Migrated some tests from ami-val and os-tests.

--- test_suite/generic/test_generic.py
+++ test_suite/generic/test_generic.py
@@ -1,5 +1,8 @@
+import pytest
+
+
 class TestsGeneric:
-    def test_bash_history_empty(self, host):
+    def test_bash_history_is_empty(self, host):
         users = [host.user().name, 'root']
 
         for u in users:
@@ -9,14 +12,65 @@ class TestsGeneric:
                 file_content_length = len(bash_history_file.content_string)
                 assert file_content_length == 0, f'{file_path} must be empty or nonexistent'
 
+    def test_console_is_redirected_to_ttys0(self, host):
+        assert host.file('/proc/cmdline').contains('console=ttyS0'), \
+            'Serial console should be redirected to ttyS0'
+
+    def test_crashkernel_is_enabled_rhel_6(self, host):
+        if float(host.system_info.release) < 7.0:
+            with host.sudo():
+                host.run_test('service kdump status')
+                assert not host.file('/proc/cmdline').contains('crashkernel'), \
+                    'crashkernel is not required as xen kdump is  not supported on RHEL 6.x'
+        else:
+            pytest.skip('RHEL is 7.x or later')
+
+    def test_crashkernel_is_enabled_rhel_7_and_above(self, host):
+        product_release_version = float(host.system_info.release)
+
+        if float(host.system_info.release) < 7.0:
+            pytest.skip('RHEL is 6.x')
+
+        if product_release_version < 9.0:
+            expected_content = 'crashkernel=auto'
+        elif host.system_info.arch == 'x86_64':
+            expected_content = 'crashkernel=1G-4G:192M,4G-64G:256M,64G-:512M'
+        else:
+            expected_content = 'crashkernel=2G-:448M'
+
+        with host.sudo(host.user().name):
+            assert host.file('/proc/cmdline').contains(expected_content), \
+                'crashkernel must be enabled in RHEL 8.x and above'
+
+
+class TestsCloudInit:
+    def test_growpart_is_present_in_config(self, host):
+        """
+        BugZilla 966888
+        """
+        assert host.file('/etc/cloud/cloud.cfg').contains('- growpart'), \
+            'growpart must be present in cloud_init_modules'
+
+    def test_wheel_group_not_set_to_default_user(self, host):
+        """
+        BugZilla 1549638
+        """
+        assert host.file('/etc/cloud/cloud.cfg').contains('wheel'), \
+            'wheel should not be configured as default_user group'
+
+
+class TestsNetworking:
+    def test_dns_resolving_works(self, host):
+        host.run_test('ping -c 5 google-public-dns-a.google.com')
+
 
 class TestsSecurity:
     def test_firewalld_is_disabled(self, host):
         product_version = 7.0
         if float(host.system_info.release) < product_version:
             for s in ['iptables', 'ip6tables']:
-                assert not host.service(s).is_enabled,\
+                assert not host.service(s).is_enabled, \
                     f'{s} service should be disabled in RHEL below {product_version}'
         else:
-            assert not host.package('firewalld').is_installed,\
+            assert not host.package('firewalld').is_installed, \
                 f'firewalld should not be installed in cloud images for RHEL {product_version} and above'
