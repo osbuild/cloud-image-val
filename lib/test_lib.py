ffa5610e6d344b2b2ccd83649e0fb1feebfda839
Nicolás Muñoz <nmunoz@redhat.com>
2022-06-22 12:19:52 +0200

    Added function to copy local file to remote host, in ssh_lib. Also added function to run local script in remote host, in test_lib.

--- lib/test_lib.py
+++ lib/test_lib.py
@@ -1,3 +1,7 @@
+from os import path
+from lib import ssh_lib
+
+
 def is_rhel_atomic_host(host):
     with host.sudo():
         return host.file('/etc/redhat-release').contains('Atomic')
@@ -15,3 +19,20 @@ def __test_keyword_in_repositories(host, keyword):
     with host.sudo():
         if host.exists('yum'):
             return keyword in host.run('yum repolist 2>&1').stdout
+
+
+def run_local_script_in_host(host, script_relative_path):
+    """
+    Runs a local script in the given remote host.
+    To achieve this, the script is first copied to the remote host.
+    :param host: The host object from the pytest test case.
+    :param script_relative_path: Relative file path of the script to run (from project's root dir)
+    :return: testinfra.backend.base.CommandResult containing: command, exit_status, stdout, stderr
+    """
+    script_remote_path = f'/tmp/{path.basename(script_relative_path)}'
+
+    ssh_lib.copy_file_to_host(host, script_relative_path, script_remote_path)
+
+    with host.sudo():
+        host.run_test(f'chmod +x "{script_remote_path}"')
+        return host.run(script_remote_path)
