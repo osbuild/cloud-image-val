7e0d1f2a31dabbeec0e9ee1c13589a6362adbc9d
jabia99 <jabia@redhat.com>
2023-05-10 11:09:24 +0200

    Add jira_skip marker and refactor conftest code
    Add a new marker that skips a test if the Jira ticket(s) provided is/are
    not closed.
    
    Also refactor the code inside conftests check_markers function

--- test_suite/conftest.py
+++ test_suite/conftest.py
@@ -2,6 +2,11 @@ import json
 import time
 import pytest
 import re
+import os
+import requests
+
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
 from py.xml import html
 from pytest_html import extras
@@ -9,78 +14,131 @@ from pytest_html import extras
 from lib import test_lib
 
 
-@pytest.fixture(autouse=True, scope='function')
-def check_markers(host, request):
-    skip_message = "This test doesn't apply to {distro_version}"
+def __get_host_info(host):
+    host_info = {}
+    host_info['distro'] = host.system_info.distribution
+    host_info['version'] = float(host.system_info.release)
+    host_info['distro_version'] = f'{host_info["distro"]}{host_info["version"]}'
+    if host_info['distro'] in ['centos', 'fedora'] and host_info['distro_version'][-2:] == '.0':
+        host_info['distro_version'] = host_info['distro_version'][:-2]
+    host_info['skip_message'] = f'This test doesn\'t apply to {host_info["distro_version"]}'
+    return host_info
 
-    def parse_distro_version(distro_version):
-        return float((re.search(r"\d+(\.\d+)?", distro_version)).group(0))
 
+def __parse_distro_version(distro_version):
+    res = re.search(r'\d+(\.\d+)?', distro_version)
+    if res:
+        return float(res.group(0))
+
+
+def __check_wait_marker(request):
     # Check if test needs to wait before being run
     wait_marker = request.node.get_closest_marker('wait')
+
     if wait_marker:
         seconds_to_wait = int(wait_marker.args[0])
         print(f'Waiting {seconds_to_wait} seconds before running test...')
         time.sleep(seconds_to_wait)
 
-    # This part checks the list of combinations of distro-version each test has.
-    # If the host doesn't meet any of the combinations, the test will be skipped.
-    run_on_marker = request.node.get_closest_marker('run_on')
+
+def __check_exclude_on_marker(request, host_info):
     exclude_on_marker = request.node.get_closest_marker('exclude_on')
-    if not run_on_marker and not exclude_on_marker:
-        pytest.fail('All test cases have to be marked with at least "run_on" \
-                    or "exlude_on" marker')
+    if not exclude_on_marker:
+        return
 
-    supported_distros_and_versions = [
-        'fedora', 'fedora34', 'fedora35', 'fedora36',
-        'rhel', 'rhel7.9', 'rhel8.4', 'rhel8.5', 'rhel8.6', 'rhel9.0',
-        'centos', 'centos8', 'centos9'
-    ]
+    exclude_on_marker_list = exclude_on_marker.args[0]
 
-    host_distro = host.system_info.distribution
-    host_version = float(host.system_info.release)
-    distro_version = f'{host_distro}{host_version}'
+    if host_info['distro'] in exclude_on_marker_list or host_info['distro_version'] in exclude_on_marker_list:
+        pytest.skip(host_info['skip_message'])
 
-    # Skip the test if the distro is explicitly excluded
-    if exclude_on_marker:
-        exclude_on_marker_list = exclude_on_marker.args[0]
-        if host_distro in exclude_on_marker_list \
-                or distro_version in exclude_on_marker_list:
-            pytest.skip(skip_message.format(distro_version=distro_version))
-        else:
-            for item in exclude_on_marker_list:
-                if host_distro in item and item[0] == '<':
-                    if host_version < parse_distro_version(item):
-                        pytest.skip(skip_message.format(distro_version=distro_version))
-                    else:
-                        return
-                if host_distro in item and item[0] == '>':
-                    if host_version > parse_distro_version(item):
-                        pytest.skip(skip_message.format(distro_version=distro_version))
-                    else:
-                        return
+    # Check if the current distro_version matches any condition of a marker element
+    # with a relational operator. If it does, do not run the test
+    for item in exclude_on_marker_list:
+        if host_info['distro'] in item:
+            item_distro_verion = __parse_distro_version(item)
 
-    # If there is no run_on_marker and distro is not excluded execute supported
-    # tests
+            if item[0] == '<' and host_info['version'] < item_distro_verion:
+                pytest.skip(host_info['skip_message'])
+
+            if item[0] == '>' and host_info['version'] > item_distro_verion:
+                pytest.skip(host_info['skip_message'])
+
+            if item[1] == '=' and host_info['version'] == item_distro_verion:
+                pytest.skip(host_info['skip_message'])
+
+
+def __check_run_on_marker(request, host_info):
+    run_on_marker = request.node.get_closest_marker('run_on')
     if not run_on_marker:
-        if distro_version in supported_distros_and_versions:
-            return
-        pytest.fail(f"{distro_version} is not supported distro/version")
+        pytest.fail('All test cases have to be marked with the "run_on" marker. Check README.md for more information.')
 
     run_on_marker_list = run_on_marker.args[0]
 
-    if 'all' in run_on_marker_list:
+    if host_info['distro'] in run_on_marker_list or \
+            host_info['distro_version'] in run_on_marker_list or \
+            'all' in run_on_marker_list:
         return
 
-    # Make the test fail if one or more items in "run_on" marker are incorrect
-    if not set(run_on_marker_list) <= set(supported_distros_and_versions):
-        pytest.fail('One or more run_on markers are not supported')
+    # Check if the current distro_version matches at least one condition of a marker element
+    # with a relational operator. If no element matches, we do not run the test
+    for item in run_on_marker_list:
+        if host_info['distro'] in item:
+            item_distro_verion = __parse_distro_version(item)
 
-    if host_distro in run_on_marker_list \
-            or distro_version in run_on_marker_list:
+            if item[0] == '<' and host_info['version'] < item_distro_verion:
+                return
+
+            if item[0] == '>' and host_info['version'] > item_distro_verion:
+                return
+
+            if item[1] == '=' and host_info['version'] == item_distro_verion:
+                return
+
+    pytest.skip(host_info['skip_message'])
+
+
+def __check_jira_skip_marker(request):
+    jira_skip_marker = request.node.get_closest_marker('jira_skip')
+    if not jira_skip_marker:
         return
-    else:
-        pytest.skip(skip_message.format(distro_version=distro_version))
+
+    jira_skip_marker_list = jira_skip_marker.args[0]
+
+    s = requests.Session()
+    retries = Retry(total=3, backoff_factor=3)
+    s.mount('https://issues.redhat.com', HTTPAdapter(max_retries=retries))
+
+    JIRA_PAT = os.getenv('JIRA_PAT')
+    if not JIRA_PAT:
+        exit('JIRA_PAT was not found')
+
+    headers = {'Authorization': f'Bearer {os.getenv("JIRA_PAT")}'}
+    endpoint_base = 'https://issues.redhat.com/rest/api/2/issue/'
+
+    for ticket_id in jira_skip_marker_list:
+        endpoint = endpoint_base + ticket_id
+        res = s.get(endpoint, headers=headers)
+
+        if res.status_code != 200:
+            print(f'ERROR: (JIRA API) - Could not check Jira ticket {ticket_id}\n'
+                  f'API request error code {res.status_code}\n'
+                  'Running test as the status cannot be checked')
+
+        status = res.json()['fields']['status']['name']
+        if status != 'Closed':
+            pytest.skip(f'Test skipped because Jira ticket {ticket_id} is not Closed yet')
+        else:
+            print(f'WARNING: Jira ticket {ticket_id} is already closed. Please remove it from the marker and put it in the docstring')
+
+
+@pytest.fixture(autouse=True, scope='function')
+def check_markers(host, request):
+    host_info = __get_host_info(host)
+
+    __check_wait_marker(request)
+    __check_exclude_on_marker(request, host_info)
+    __check_run_on_marker(request, host_info)
+    __check_jira_skip_marker(request)
 
 
 @pytest.mark.run_on(['rhel'])
@@ -165,17 +223,17 @@ def pytest_runtest_makereport(item, call):
     outcome = yield
     report = outcome.get_result()
 
-    # Set the test cases "Duration" format
+    # Set the test cases 'Duration' format
     setattr(report, 'duration_formatter', '%S.%f sec')
 
-    # Fill "Test Case" column
+    # Fill 'Test Case' column
     report.test_case = f'{str(item.parent.name)}::{str(item.function.__name__)}'
 
-    # Fill "Description" column
+    # Fill 'Description' column
     description_text = __truncate_text(str(item.function.__doc__), 120)
     report.description = description_text
 
-    # Fill "Image Reference" column
+    # Fill 'Image Reference' column
     if 'instance_data' in item.funcargs:
         instance = item.funcargs['instance_data']
         image_ref = instance['ami'] if 'ami' in instance else instance['image']
