d6e970a7174f2e24f4982b8332d89e06562a29c0
Alexander Todorov <atodorov@redhat.com>
2023-08-09 10:43:32 +0300

    Add a test to assert on /etc/machine-id permissions for AWS
    Related: RHBZ #2221269

--- test_suite/cloud/test_aws.py
+++ test_suite/cloud/test_aws.py
@@ -29,6 +29,14 @@ def instance_data_aws_cli(host, instance_data_aws_web):
 
 @pytest.mark.order(2)
 class TestsAWS:
+    @pytest.mark.run_on(['rhel', 'fedora'])
+    def test_etc_machine_id_permissions(self, host, instance_data):
+        """
+        Check that /etc/machine-id permissions are 444.
+        Bugzilla: 2221269
+        """
+        assert host.file("/etc/machine-id").mode == 0o444, "Expected 444 permissions for /etc/machine-id"
+
     # TODO: Divide test. Analyze centos
     @pytest.mark.pub
     @pytest.mark.run_on(['rhel', 'fedora'])
