1cc5fb63520fa63d727372fa2ad62b8ae5c44244
sshmulev <sshmulev@redhat.com>
2024-07-24 10:16:38 +0300

    Pass the json file path as function arg

--- test_suite/generic/helpers.py
+++ test_suite/generic/helpers.py
@@ -1,10 +1,12 @@
 import os
 import json
 
+INSTANCES_JSON_PATH = os.environ['CIV_INSTANCES_JSON']
 
-def __get_instance_data_from_json(key_to_find, values_to_find):
-    with open(os.environ['CIV_INSTANCES_JSON'], 'r') as f:
+
+def __get_instance_data_from_json(key_to_find, values_to_find, path=INSTANCES_JSON_PATH):
+    with open(path, 'r') as f:
         instances_json_data = json.load(f)
-    for instance in instances_json_data.values():
-        if key_to_find in instance.keys() and instance[key_to_find] in values_to_find:
-            return instance
+        for instance in instances_json_data.values():
+            if key_to_find in instance.keys() and instance[key_to_find] in values_to_find:
+                return instance
