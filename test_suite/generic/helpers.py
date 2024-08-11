14fa02b72c02814b2c4296043cd1b421b612f108
sshmulev <sshmulev@redhat.com>
2024-08-11 09:57:24 +0300

    Extend Opentelemetry-Collector testing with SELinux policies
    https://issues.redhat.com/browse/CLOUDX-1005

--- test_suite/generic/helpers.py
+++ test_suite/generic/helpers.py
@@ -1,5 +1,6 @@
 import os
 import json
+from lib import test_lib
 
 INSTANCES_JSON_PATH = os.environ['CIV_INSTANCES_JSON']
 
@@ -10,3 +11,14 @@ def __get_instance_data_from_json(key_to_find, values_to_find, path=INSTANCES_JS
     for instance in instances_json_data.values():
         if key_to_find in instance.keys() and instance[key_to_find] in values_to_find:
             return instance
+
+
+def check_avc_denials(host):
+    command_to_run = 'x=$(ausearch -m avc 2>&1 &); echo $x'
+    result = test_lib.print_host_command_output(host,
+                                                command_to_run,
+                                                capture_result=True)
+
+    no_avc_denials_found = 'no matches' in result.stdout
+
+    assert no_avc_denials_found, 'There should not be any avc denials (selinux)'
--- test_suite/generic/test_generic.py
+++ test_suite/generic/test_generic.py
@@ -1,9 +1,9 @@
 import os
 import re
 import time
-
 import pytest
 from packaging import version
+from test_suite.generic import helpers
 
 from lib import console_lib
 from lib import test_lib
@@ -42,14 +42,7 @@ class TestsGeneric:
         if instance_data['cloud'] == 'azure':
             pytest.skip('Skipping on fedora due to old image definitions')
 
-        command_to_run = 'x=$(ausearch -m avc 2>&1 &); echo $x'
-        result = test_lib.print_host_command_output(host,
-                                                    command_to_run,
-                                                    capture_result=True)
-
-        no_avc_denials_found = 'no matches' in result.stdout
-
-        assert no_avc_denials_found, 'There should not be any avc denials (selinux)'
+        helpers.check_avc_denials(host)
 
     @pytest.mark.run_on(['all'])
     def test_bash_history_is_empty(self, host):
