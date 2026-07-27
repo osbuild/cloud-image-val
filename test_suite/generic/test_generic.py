5924e4cc686f032242fe0746932687719f3d0193
Pranav Kesavarapu <prkesava@redhat.com>
2026-07-27 09:29:16 +0530

    fix(oci): update sshd, driver and /boot tests
    update sshd to check for uncommented PasswordAuthentication yes
    update network driver to check the correct path
    add oci as a case for /boot drive as it's lvm only

--- test_suite/generic/test_generic.py
+++ test_suite/generic/test_generic.py
@@ -335,13 +335,14 @@ class TestsGeneric:
         release_major = version.parse(host.system_info.release).major
         is_aarch64 = host.system_info.arch == 'aarch64'
         is_azure = instance_data['cloud'] == 'azure'
+        is_oci = instance_data['cloud'] == 'oci'
         lvm_check = host.run("lsblk -f | grep LVM").rc == 0
         is_fedora = host.system_info.distribution == 'fedora'
 
         if (
            (release_major == 8 and is_aarch64)
            or (release_major == 9)
-           or (release_major >= 10 and is_azure and lvm_check)
+           or (release_major >= 10 and (is_azure or is_oci) and lvm_check)
            or is_fedora
            ):
             assert host.mount_point("/boot").exists, "/boot mount is missing"
